9c507d2dd5304872ef24ee1ced39c3e5b5df4d43
OllieMilky <oliver-hall@hotmail.co.uk>
2021-10-06 16:07:38 +0100

    First 3 chapters up to composition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const expenses = [
+		{
+			id: "e1",
+			title: "Toilet Paper",
+			amount: 94.12,
+			date: new Date(2020, 7, 14),
+		},
+		{
+			id: "e2",
+			title: "Booze",
+			amount: 912,
+			date: new Date(2020, 3, 14),
+		},
+		{
+			id: "e3",
+			title: "Taxi",
+			amount: 542,
+			date: new Date(2020, 1, 14),
+		},
+		{
+			id: "e4",
+			title: "Food",
+			amount: 52,
+			date: new Date(2020, 7, 16),
+		},
+	];
+	return (
+		<div>
+			<ExpenseList expenses={expenses} />
+		</div>
+	);
 }
 
 export default App;
